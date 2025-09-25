0b2e0b78edfad050edc4d886cbc8ee0ea53196be
Andy Jones <andy.jones@digital.education.gov.uk>
2025-09-25 14:54:46 +0100

    Adding azure storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,19 @@
-export default () => ({
+export default ({ env }) => ({
   'schema-visualizer': {
     enabled: true,
+  },
+
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-azure-storage',
+      providerOptions: {
+        account: env('AZURE_STORAGE_ACCOUNT'),
+        accountKey: env('AZURE_STORAGE_ACCOUNT_KEY'),
+        serviceBaseURL: env('AZURE_STORAGE_URL'),
+        containerName: env('AZURE_STORAGE_CONTAINER_NAME'),
+        defaultPath: 'uploads',
+        cdnBaseURL: env('AZURE_CDN_URL'), 
+      },
+    },
   },
 });
